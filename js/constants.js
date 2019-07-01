9a3ac330be6c1362fb01d7943333f98853398294
Margarita <margaritk@inbox.ru>
2019-07-01 16:30:21 +0300

    Загружает данные с сервера для отрисовки главной страницы

--- js/constants.js
+++ js/constants.js
@@ -5,6 +5,7 @@
     ESC_KEYCODE: 27,
     ENTER_KEYCODE: 13,
     MULTIPLIER_DIVISOR_ON_HUNDRED_PARTS: 100,
-    MAX_PERCENT_INPUT_VALUE: 100
+    MAX_PERCENT_INPUT_VALUE: 100,
+    URL_GET: 'https://js.dump.academy/kekstagram/data'
   };
 })();
--- js/gallery.js
+++ js/gallery.js
@@ -1,68 +1,22 @@
 'use strict';
 
 (function () {
-  var AVATARS_COUNT = 6;
-  var PHOTOS_COUNT = 25;
-  var MIN_RANDOM_VALUE = 15;
-  var MAX_RANDOM_VALUE = 200;
-  var NAMES = ['Виктор', 'Елена', 'Катя', 'Павел', 'Олеся', 'Инна', 'Алла'];
-  var COMMENTS = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-
   var usersPhotosSection = document.querySelector('.pictures');
-  var usersPhotos = [];
 
-  var generateCommentObject = function (namesArr, commentsArr, count) {
-    var userComment = {};
-    count = count || AVATARS_COUNT;
 
-    if (namesArr.length > 0 && commentsArr.length > 0) {
-      userComment = {
-        name: window.utils.getRandomValue(namesArr),
-        message: window.utils.getRandomValue(commentsArr),
-        avatar: 'img/avatar-' + Math.round(0.5 + Math.random() * count) + '.svg'
-      };
-    }
+  var onErrorStartPage = function (errorMessage) {
+    var errorStartPageTemplate = document.querySelector('#error__start-page').content.querySelector('.error');
+    var errorStartPagePopup = errorStartPageTemplate.cloneNode(true);
+    var errorText = errorStartPagePopup.querySelector('.error__title');
+    var errorButton = errorStartPagePopup.querySelector('.error__button');
 
-    return userComment;
-  };
+    errorText.textContent = errorMessage;
+    document.body.appendChild(errorStartPagePopup);
 
-  var generateCommentsArray = function (min, max) {
-    var userComments = [];
-    min = min || 0;
-
-    if (max) {
-      for (var i = 0; i < Math.round(min - 0.5 + Math.random() * (max - min + 1)); i++) {
-        userComments[i] = [
-          generateCommentObject(NAMES, COMMENTS, AVATARS_COUNT)
-        ];
-      }
-    }
-
-    return userComments;
-  };
-
-  var generateUsersPhotos = function (min, max, count) {
-    min = min || 0;
-    count = count || PHOTOS_COUNT;
-
-    if (max) {
-      for (var i = 0; i < count; i++) {
-        usersPhotos[i] = {
-          url: 'photos/' + (i + 1) + '.jpg',
-          likes: Math.round(min - 0.5 + Math.random() * (max - min + 1)),
-          comments: generateCommentsArray(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
-        };
-      }
-    }
-
-    return usersPhotos;
+    errorButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      document.body.removeChild(errorStartPagePopup);
+    });
   };
 
   var createUserPhoto = function (photo) {
@@ -88,12 +42,12 @@
         fragment.appendChild(createUserPhoto(photosArr[i]));
       }
 
-      return fragment;
+      usersPhotosSection.appendChild(fragment);
     }
 
     return {};
   };
 
-  usersPhotos = generateUsersPhotos(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE, PHOTOS_COUNT);
-  usersPhotosSection.appendChild(addUsersPhotos(usersPhotos));
+
+  window.backend.load(window.constants.URL_GET, addUsersPhotos, onErrorStartPage);
 })();
